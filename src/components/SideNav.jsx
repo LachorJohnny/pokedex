b0d097f1dd56943af71ac0737f86a1216cc3d9ff
Jonatan Lachor <lachorjonatan@icloud.com>
2025-03-23 23:31:28 +0100

    Add searching functionality for side nav input

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,22 +1,35 @@
 import { first151Pokemon, getFullPokedexNumber } from '../utils';
+import { useState } from 'react';
 
 const SideNav = (props) => {
   const { selectedPokemon, setSelectedPokemon } = props;
+  const [searchValue, setSearchValue] = useState('');
+
+  const filteredPokemon = first151Pokemon.filter((pokemon, pokemonIndex) => {
+    if (getFullPokedexNumber(pokemonIndex).includes(searchValue)) return true;
+    if (pokemon.toLowerCase().includes(searchValue.toLowerCase())) return true;
+    return false;
+  });
 
   return (
     <nav>
       <div className="header">
         <h1 className="text-gradient">Pokédex</h1>
       </div>
-      <input type="text" />
-      {first151Pokemon.map((pokemon, pokemonIndex) => {
+      <input
+        type="text"
+        placeholder='E.g. 001 or Bulba...'
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
+      {filteredPokemon.map((pokemon, pokemonIndex) => {
         return (
           <button
             key={pokemonIndex}
             className={'nav-card ' + (pokemonIndex === selectedPokemon ? 'nav-card-selected' : '')}
-            onClick={() => setSelectedPokemon(pokemonIndex)}
+            onClick={() => setSelectedPokemon(first151Pokemon.indexOf(pokemon))}
           >
-            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+            <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
             <p>{pokemon}</p>
           </button>
         );
